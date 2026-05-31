caefc0b04ac454c36dd0f114ab5617a27c87ffec
vahe2002pog <vahepogosyan@gmail.com>
2026-05-31 10:32:36 +0500

    fix: Update Inno Compiler path to use environment variable for better portability

--- installer/build_installer.ps1
+++ installer/build_installer.ps1
@@ -32,7 +32,7 @@ function Resolve-InnoCompiler {
         $candidates += $env:INNO_COMPILER_PATH
     }
     $candidates += @(
-        "C:\Users\vahep\AppData\Local\Programs\Inno Setup 6\ISCC.exe",
+        "$env:LOCALAPPDATA\Programs\Inno Setup 6\ISCC.exe",
         "${env:ProgramFiles(x86)}\Inno Setup 6\ISCC.exe",
         "$env:ProgramFiles\Inno Setup 6\ISCC.exe"
     )
